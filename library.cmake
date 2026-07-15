13b1ca1454cd015a2a6d56a5e60b5efaba40c2a4
Kevin F. Ortega <kevin.f.ortega@jpl.nasa.gov>
2026-07-15 11:55:30 -0700

    adding gr740 platform in restricts. This is a workaround until fprime has a better way of restricting platforms. Also removed unnecessary checks on platforms at lower levels. They are unnecessary because the higher level already checks

--- library.cmake
+++ library.cmake
@@ -1,6 +1,6 @@
 # Allow config to be included regardless of platform
 add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/VxWorks/library/config")
 
-restrict_platforms(vxworks)
+restrict_platforms(vxworks vswir-gr740)
 add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/VxWorks/Os")
 add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/VxWorks/Svc")
